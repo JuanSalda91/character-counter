cb6215fa5874fdec0c3b8539b545f724562ac050
JuanSalda91 <juansalda9131@gmail.com>
2026-01-05 09:53:42 -0500

    file updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import { CharacterCounter } from './components/CharacterCounter/CharacterCounter';
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <CharacterCounter minwords={25} maxwords={100} targetReadingTime={3} />
+  );
 }
 
-export default App
+export default App;
