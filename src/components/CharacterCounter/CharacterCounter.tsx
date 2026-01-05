e34764c35ccc5e753dfc35cee1862b190c9b853a
JuanSalda91 <juansalda9131@gmail.com>
2026-01-05 09:51:50 -0500

    component init

---
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { CharacterCounterProps, TextStats } from '../../types';
+import { TextInput } from '../TextInput/TextInput';
+import { StatsDisplay } from '../StatsDisplay/StatsDisplay';
